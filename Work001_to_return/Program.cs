bc2c22a5d7e1e0d30879e29f90957dcbccc8b2fd
Egor <edzumaev91@gmail.com>
2023-10-27 21:23:33 +0300

    install commit

---
+++ Work001_to_return/Program.cs
@@ -0,0 +1,34 @@
+﻿// // Напишите программу, которая на вход принимает число (N), 
+// // а на выходе показывает все чётные числа от 1 до N.
+// // using System.Xml.XPath;
+
+// void OutValue(int N)
+// {
+
+// for (int i = 1; i < N; i++) 
+// {
+   
+//     if (i % 2 == 0 )
+//     {
+    
+//         Console.Write($"{i},");
+//     }
+// }
+// }
+// Console.Clear();
+// Console.WriteLine("Введите число");
+// int value = Convert.ToInt32(Console.ReadLine());
+// OutValue(value);
+// Console.Clear();
+// Console.WriteLine("Введите число ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// void SumVAlueR(int M)
+// {
+// for(int i=1;i<=M;i++)
+// {
+// if (i % 2 == 0)
+// System.Console.Write(i + ", " );
+// }
+// }
+// SumVAlueR(n);
+
